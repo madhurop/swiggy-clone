d6ab78134da75f3562e468a49e8391d97698b250
madhurop <madhurborade654@gmail.com>
2024-05-02 16:23:40 +0530

    React Toastify

--- src/Components/Error.jsx
+++ src/Components/Error.jsx
@@ -1,14 +1,30 @@
 import React from 'react'
 import { useRouteError } from 'react-router-dom'
-function Error() {
-    const err = useRouteError()
+
+
+const Error = () => {
+  const error=useRouteError()
+  
   return (
-    <div>
-      <h1>Error</h1>
-      <h1>{err.status}</h1>
-      <h2>Something get wrong!!!!!</h2>
+    <div className="min-h-screen bg-gray-100 flex flex-col justify-center">
+      <div className="max-w-md w-full mx-auto">
+        <div className="text-center">
+          <h1 className="text-4xl font-semibold text-gray-800 mb-4">Oops!</h1>
+          <p className="text-xl text-gray-600 mb-8">Something went wrong...</p>
+          <p className="text-lg text-gray-700 mb-4">{error.status}</p>
+          
+          <button
+            onClick={() => window.location.reload()}
+            className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+          >
+            Refresh Page
+          </button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
+
+
 
 export default Error
--- src/Components/MainBody.jsx
+++ src/Components/MainBody.jsx
@@ -3,13 +3,27 @@ import FoodTypes from './FoodTypes'
 import RestaurantChain from './RestaurantChain'
 import { useState,useEffect } from 'react'
 import useOnlineStatus from '../utils/useOnlineStatus'
+import { ToastContainer,toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
 
 function MainBody() {
-  const onlineStatus = useOnlineStatus()
+  const onlineStatus = useOnlineStatus();
+  const [mode, setMode] = useState(onlineStatus ? 'Online' : 'Offline');
+
+  useEffect(() => {
+    const notify = () => toast('You Are ' + mode);
+    notify();
+  }, [mode]);
+
+  useEffect(() => {
+    setMode(onlineStatus ? 'Online' : 'Offline');
+  }, [onlineStatus]);
+  
   console.log(onlineStatus)
   
   return (
     <div className="flex flex-col items-center  w-full md:screen h-full bg-white mt-2">
+      <ToastContainer/>
       
       <FoodTypes/>
       {/* <RestaurantChain ch="flex-no-wrap overflow-x-auto"/> */}
